e1fc70e8ed7949659b4447c2760e2b1f623a4525
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 19:40:03 -0400

    debian

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -97,9 +97,6 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install build-essen
     xsltproc
 #docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y upgrade
 
-GDK_PIX_VER="2.40.0+dfsg-5"
-PKG_SRC=https://dl.cloudsmith.io/public/${PRG_REPO}/main
-
 docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source; dpkg-buildpackage -b -uc -us -j4; mkdir dist; mv ../*.deb dist; chmod -R a+rw dist"
 
