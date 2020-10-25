70587abca536ae1217d88096cb046be33caadbbb
mgrouch <mgrushinskiy@gmail.com>
2020-10-25 16:40:40 -0400

    debian

--- .circleci/build-debian.sh
+++ .circleci/build-debian.sh
@@ -17,13 +17,13 @@ if [ "$EMU" = "on" ]; then
   if [ "$CONTAINER_DISTRO" = "raspbian" ]; then
       docker run --rm --privileged --cap-add=ALL multiarch/qemu-user-static:register --reset
   else
-      docker run --rm --privileged --security-opt="seccomp=unconfined" --cap-add=ALL multiarch/qemu-user-static --reset --credential yes --persistent yes
+      docker run --rm --privileged --cap-add=ALL --security-opt="seccomp=unconfined" multiarch/qemu-user-static --reset --credential yes --persistent yes
   fi
 fi
 
 WORK_DIR=$(pwd):/ci-source
 
-docker run --privileged --security-opt="seccomp=unconfined" --cap-add=ALL -d -ti -e "container=docker" -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
+docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker" -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
 DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
 
 docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
